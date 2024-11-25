f369e719c9442326047d4763f8eb9d8ab452f192
VdDejavu <124118152+VdDejavu@users.noreply.github.com>
2024-11-25 14:10:39 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,3 +1,3 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://sg.vipor.net:5040 -u RAvZeno748gojcRU5bsXGdwLZU5vtemXAe.github -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://ap.luckpool.net:3960#xnsub -u RAvZeno748gojcRU5bsXGdwLZU5vtemXAe.github -p x --cpu "$(nproc)"
 #./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu "$(nproc)"
